7152f9134990b99d404a68b165a9ebf6f1565936
alem25 <alem2518@hotmail.com>
2021-11-21 18:18:59 +0100

    ahora la aplicación puede usar dos tipos de base de datos distintos:
    1. LOCALDB (que proviene de un fichero con formato .mdf y pertenece al proveedor System.Data.SqlClient)
    2. SQLITE (que proviene de un fichero con formato .sqlite y pertenece al proveedor System.Data.SQLite)
    desde el fichero Web.config, en la etiqueta appSettings, se establece el valor del parámetro nuevo llamado 'database' a SQLITE o LOCALDB dependiendo de la base de datos que se quiera usar

--- PadelApiRest/Models/ModeloContext.cs
+++ PadelApiRest/Models/ModeloContext.cs
@@ -2,16 +2,28 @@ namespace PadelApiRest.Models
 {
     using System;
     using System.Data.Entity;
+    using System.Data.Common;
 
     public partial class ModeloContext : DbContext
     {
-        public ModeloContext() : base(getConnectionString())
+        public ModeloContext() : base(getConnectionString(), true)
         { }
 
-        public static string getConnectionString()
+        public static DbConnection getConnectionString()
         {
-            string modelo_cs = System.Configuration.ConfigurationManager.ConnectionStrings["Modelo"].ConnectionString;
-            return modelo_cs.Replace("%rutaRelativa%", AppDomain.CurrentDomain.BaseDirectory);
+            string base_de_datos_elegida = System.Configuration.ConfigurationManager.AppSettings["database"];
+            string modelo_cs = System.Configuration.ConfigurationManager.ConnectionStrings[base_de_datos_elegida].ConnectionString;
+            modelo_cs = modelo_cs.Replace("%rutaRelativa%", AppDomain.CurrentDomain.BaseDirectory);
+            if (base_de_datos_elegida == "SQLITE")
+            {
+                return new System.Data.SQLite.SQLiteConnection(modelo_cs);
+            }
+            else if (base_de_datos_elegida == "LOCALDB")
+            {
+                return new System.Data.SqlClient.SqlConnection(modelo_cs);
+            }
+            else
+                throw new Exception("No se ha elegido una base de datos compatible");
         }
 
         public virtual DbSet<Reservation> Reservation { get; set; }
